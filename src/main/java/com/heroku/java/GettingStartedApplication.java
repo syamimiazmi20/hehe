51f3a5347129d50e0466cdf7b32ca3350dbdd65d
DamDam42 <2022464546@student.uitm.edu.my>
2024-06-11 23:45:12 +0800

    aaaaaaaaa

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -1,15 +1,16 @@
 package com.heroku.java;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.util.ArrayList;
-import java.util.Map;
 
 @SpringBootApplication
 @Controller
@@ -30,7 +31,7 @@ public class GettingStartedApplication {
         public String addticket(){
             return "createBooking";
         }
-    }
+    
 
     @GetMapping("/database")
     String database(Map<String, Object> model) {

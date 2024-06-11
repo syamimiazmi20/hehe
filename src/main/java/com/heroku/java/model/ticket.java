160c654e1d227b36c6b0f4083bcd546e5174044a
DamDam42 <2022464546@student.uitm.edu.my>
2024-06-12 02:07:05 +0800

    a

--- src/main/java/com/heroku/java/model/ticket.java
+++ src/main/java/com/heroku/java/model/ticket.java
@@ -1,27 +1,27 @@
 package com.heroku.java.model;
 
 public class ticket {
-    private String tickettype;
-    private double ticketprice;
+    private String ticketType;
+    private double ticketPrice;
     private int ticketid;
 
     public ticket() {
     }
 
     public String getTicketType() {
-        return tickettype;
+        return ticketType;
     }
 
-    public void setTicketType(String tickettype) {
-        this.tickettype = tickettype;
+    public void setTicketType(String ticketType) {
+        this.ticketType = ticketType;
     }
 
     public double getTicketPrice() {
-        return ticketprice;
+        return ticketPrice;
     }
 
-    public void setTicketPrice(double ticketprice) {
-        this.ticketprice = ticketprice;
+    public void setTicketPrice(double ticketPrice) {
+        this.ticketPrice = ticketPrice;
     }
 
     public int getTicketId() {

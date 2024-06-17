c1b3c2469b982bff5b74fc94340a165b1bb91564
DamDam42 <2022464546@student.uitm.edu.my>
2024-06-18 02:32:34 +0800

    a

--- src/main/java/com/heroku/java/controller/TicketController.java
+++ src/main/java/com/heroku/java/controller/TicketController.java
@@ -126,11 +126,13 @@ public String updateTicket(@ModelAttribute("updateTickets") ticket ticket, Model
         Connection connection = dataSource.getConnection();
         String sql = "UPDATE ticket SET tickettype=? , ticketprice=? WHERE ticketid=?";
         final var statement=connection.prepareStatement(sql);
+        Long ticketID = ticket.getTicketId();
         String ticketType = ticket.getTicketType();
         double ticketPrice = ticket.getTicketPrice();
 
-        statement.setString(1,ticketType);
-        statement.setDouble(2,ticketPrice);
+        statement.setLong(3, ticketID);
+        statement.setString(2,ticketType);
+        statement.setDouble(3,ticketPrice);
 
         statement.executeUpdate();
 
